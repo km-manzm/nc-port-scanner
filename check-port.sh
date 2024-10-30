d4936cf55a081f90d40e1c447ade63d5a2cc19c4
Manuel Manz <60424464+km-manzm@users.noreply.github.com>
2024-10-30 10:24:58 +0100

    Create check-port.sh

---
+++ check-port.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+function checkport {
+	if nc -zv -w30 $1 $2 <<< '' &> /dev/null
+	then
+		echo "[+] Port $1/$2 is open"
+	else
+		echo "[-] Port $1/$2 is closed"
+	fi
+}
+
+# Example checks
+for i in 172.22.228.{1..255} 
+do
+checkport $i 22
+done
