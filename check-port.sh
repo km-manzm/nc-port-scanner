1db5495e10c6ae0b49f4e88745f28e9f50f03ad4
Manuel Manz <60424464+km-manzm@users.noreply.github.com>
2024-10-30 11:09:47 +0100

    Update check-port.sh

--- check-port.sh
+++ check-port.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-function checkport {
-	if nc $1 $2 <<< '' &> /dev/null
-	then
-		echo "[+] Port $1/$2 is open"
-	else
-		echo "[-] Port $1/$2 is closed"
-	fi
-}
-
-# Example checks
+# check for ssh server
 for i in 172.22.228.{1..255} 
 do
 # checkport $i 22
